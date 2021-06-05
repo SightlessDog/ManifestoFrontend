bdf289a119f20c59300c4616a72f3c3487f9f100
elyess.eleuch <ely.369@outlook.com>
2021-06-05 10:34:28 +0200

    client sends to server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
 import { v1 as uuid } from "uuid";
+import axios from "axios";
 
 const App = () => {
+  let object = null;
+  let modifiedObj;
   const [canvas, setCanvas] = useState("");
+  const [submit, setSubmit] = useState(false);
 
   const initCanvas = () =>
     new fabric.Canvas("canv", {
@@ -16,30 +20,56 @@ const App = () => {
     setCanvas(initCanvas());
   }, []);
 
-  const addShape = (e) => {
-    let object;
+  useEffect(() => {
+    if (canvas) {
+      canvas.on("object:modified", function (options) {
+        if (options.target) {
+          console.log(options);
+          console.log();
+          console.log(object.getRadiusX());
+          modifiedObj = {
+            aoi: {
+              center: object.getCenterPoint(),
+              radius: object.getRadiusX(),
+            },
+            id: options.target.id,
+          };
+        }
+      });
+    }
+  }, [canvas]);
 
-    object = new fabric.Circle({
-      radius: 50,
-    });
+  const addShape = (e) => {
+    let type = e.target.name;
+
+    if (type === "circle") {
+      object = new fabric.Circle({
+        radius: 50,
+      });
+    }
 
     object.set({ id: uuid() });
     canvas.add(object);
     canvas.renderAll();
   };
 
+  const handleSubmit = () => {
+    axios
+      .post("http://localhost:8080", modifiedObj)
+      .then(() => console.log("aoi created"))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
       <div>
-        <div>
-          <button type="button" name="circle" onClick={addShape}>
-            Add a Circle
-          </button>
-        </div>
-
-        <div>
-          <canvas id="canv" />
-        </div>
+        <button type="button" name="circle" onClick={addShape}>
+          Add a Circle
+        </button>
+      </div>
+      <div>
+        <canvas id="canv" />
+        <button onClick={handleSubmit}>Submit</button>
       </div>
     </div>
   );
