07ecb24b6d521a02c91d6796adc5a866ae6198f1
elyess.eleuch <ely.369@outlook.com>
2021-06-14 20:29:24 +0200

    Interface done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,53 @@ import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
 import { v1 as uuid } from "uuid";
 import axios from "axios";
+import styled from "styled-components";
+import { blue } from "chalk";
 
 const App = () => {
   let object = null;
-  let modifiedObj;
+  const [modidiedObject, setModidiedObject] = useState("");
   const [canvas, setCanvas] = useState("");
-  const [submit, setSubmit] = useState(false);
+  const [canAdd, setCanAdd] = useState(true);
+
+  const ButtonsContainer = styled.div`
+    display: flex;
+    position: absolute;
+    width: 40%;
+    top: 90%;
+    left: 43%;
+    z-index: 99;
+    align-content: space-around;
+  `;
+
+  const StyledButton = styled.button`
+    -moz-transition: all 0.4s ease-in;
+    -o-transition: all 0.4s ease-in;
+    -webkit-transition: all 0.4s ease-in;
+    background-color: #deb866;
+    transition: all 0.4s ease-in;
+    padding: 12px 24px;
+    font-size: 1.2rem;
+    font-family: "Roboto";
+    border-radius: 6px;
+    width: 150px;
+    cursor: pointer;
+    transition: background-color 0.2 linear, color 0.2 linear;
+    &:hover {
+      background-color: #ca9b46;
+      color: #dbd9db;
+    }
+    &:active {
+      background-color: #b68438;
+      color: #dbd9db;
+    }
+  `;
 
   const initCanvas = () =>
     new fabric.Canvas("canv", {
+      width: 700,
       height: 500,
-      width: 500,
-      backgroundColor: "white",
+      backgroundColor: "red",
     });
 
   useEffect(() => {
@@ -24,16 +59,15 @@ const App = () => {
     if (canvas) {
       canvas.on("object:modified", function (options) {
         if (options.target) {
-          console.log(options);
-          console.log();
+          console.log(object.getCenterPoint());
           console.log(object.getRadiusX());
-          modifiedObj = {
+          setModidiedObject({
             aoi: {
               center: object.getCenterPoint(),
               radius: object.getRadiusX(),
             },
             id: options.target.id,
-          };
+          });
         }
       });
     }
@@ -41,35 +75,47 @@ const App = () => {
 
   const addShape = (e) => {
     let type = e.target.name;
+    if (canAdd) {
+      if (type === "circle") {
+        object = new fabric.Circle({
+          borderColor: blue,
+          radius: 50,
+        });
+      }
 
-    if (type === "circle") {
-      object = new fabric.Circle({
-        radius: 50,
-      });
+      object.set({ id: uuid() });
+      canvas.add(object);
+      canvas.renderAll();
+      setCanAdd(false);
     }
-
-    object.set({ id: uuid() });
-    canvas.add(object);
-    canvas.renderAll();
   };
 
   const handleSubmit = () => {
+    console.log(modidiedObject);
     axios
-      .post("http://localhost:8080", modifiedObj)
+      .post("http://localhost:8080", modidiedObject)
       .then(() => console.log("aoi created"))
       .catch((err) => console.log(err));
   };
 
   return (
     <div className="App">
-      <div>
-        <button type="button" name="circle" onClick={addShape}>
+      <div>position your area of interest and click on submit</div>
+      <ButtonsContainer>
+        <StyledButton type="button" name="circle" onClick={addShape}>
           Add a Circle
-        </button>
-      </div>
-      <div>
+        </StyledButton>
+        <StyledButton onClick={handleSubmit}>Submit</StyledButton>
+      </ButtonsContainer>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "200px",
+        }}
+      >
         <canvas id="canv" />
-        <button onClick={handleSubmit}>Submit</button>
       </div>
     </div>
   );
